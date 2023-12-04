e8b456d85ed52b1781fb3a24d60d8d9d24ac77a5
Roman Bondar <romanbondar72@gmail.com>
2023-12-04 10:40:57 +0200

    new commit 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit Hello world Bondar Roman <code>src/App.js</code> and save to reload.
+          Edit Hello world Bondar Roman 6666 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
